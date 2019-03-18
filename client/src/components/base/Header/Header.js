5929bf52c954e0642ad93f9669dbc47a65bcf42b
HyeongTak <andy395023@gmail.com>
2019-03-18 20:32:16 +0900

    flex설정, 버튼

--- client/src/components/base/Header/Header.js
+++ client/src/components/base/Header/Header.js
@@ -10,20 +10,36 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Modal from '@material-ui/core/Modal';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import SearchIcon from '@material-ui/icons/Search';
 
 import LockIcon from '@material-ui/icons/LockOutlined';
 
 const StyledAppBar = styled(AppBar)`
     &&{
+        display : flex;
         background-color: white;
         box-shadow: 0px 2px 0px rgba(255, 105, 135, .3);
+        text-align: right;
     }
+`
+
+const StyledButton = styled(Button)`
+    &&{
+        background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+        border-radius: 5px;
+        border: 0;
+        color: white;
+        height: 40px;
+        padding: 0 20px;
+        box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .3);
+    }
+`;
+
+
+const Grow = styled.div`
+    flex-grow: 1;
 `
 
 class Header extends Component {
@@ -43,12 +59,17 @@ class Header extends Component {
     const { classes } = this.props;
 
     return (
-        <div className>
+        <div>
           <StyledAppBar position="static" color="default">
             <Toolbar>
               <Typography variant="h6" color="inherit">
                 Paou
               </Typography>
+                <Grow/>
+                <StyledButton>
+                    + New
+                </StyledButton>
+                
             </Toolbar>
           </StyledAppBar>
         </div>
