adbce84d0c5d8ddb941622765a988bf89a1e8d8a
Dotsung <andy395023@gmail.com>
2019-04-15 23:27:27 +0900

    eslint 삭제

---
+++ client/src/components/Board/BoardSection.js
@@ -0,0 +1,12 @@
+// @flow
+import React, { Component } from 'react';
+
+class BoardSection extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default BoardSection;
